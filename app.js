14d2aba5c17b0fbc7d9808ff645342278ac2271e
hgoncerz <hubertg489655@gmail.com>
2022-06-27 17:26:24 +0200

    all types of typescript

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ["sports", "cooking"],
+    role: Role.ADMIN
 };
+var favoriteActivities;
+favoriteActivities = ["sports"];
 console.log(person.name);
+person.hobbies.map(function (hob) {
+    console.log(hob.toUpperCase());
+});
+if (person.role === Role.ADMIN) {
+    console.log("is author");
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,25 @@
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["sports", "cooking"],
+  role: Role.ADMIN,
 };
 
+let favoriteActivities: any[];
+favoriteActivities = ["sports"];
+
 console.log(person.name);
+
+person.hobbies.map((hob) => {
+  console.log(hob.toUpperCase());
+});
+
+if (person.role === Role.ADMIN) {
+  console.log("is author");
+}
