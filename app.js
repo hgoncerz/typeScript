ca994e0b6d92d2a3ae8d469eeed25de2f71c130e
hgoncerz <hubertg489655@gmail.com>
2022-06-27 17:55:32 +0200

    castom type

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Maximilian",
-    age: 30,
-    hobbies: ["sports", "cooking"],
-    role: Role.ADMIN
+// enum Role {
+//   ADMIN = 5,
+//   READ_ONLY,
+//   AUTHOR,
+// }
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: Role.ADMIN,
+// };
+// let favoriteActivities: string[];
+// favoriteActivities = ["sports"];
+// console.log(person.name);
+// person.hobbies.map((hob) => {
+//   console.log(hob.toUpperCase());
+// });
+// if (person.role === Role.ADMIN) {
+//   console.log("is author");
+// }
+var combine = function (input1, input2, resultConver) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultConver === "as-numbers") {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 };
-var favoriteActivities;
-favoriteActivities = ["sports"];
-console.log(person.name);
-person.hobbies.map(function (hob) {
-    console.log(hob.toUpperCase());
-});
-if (person.role === Role.ADMIN) {
-    console.log("is author");
-}
+var combinedAges = combine(30, 26, "as-numbers");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-numbers");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-string");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,25 +1,55 @@
-enum Role {
-  ADMIN = 5,
-  READ_ONLY,
-  AUTHOR,
-}
+// enum Role {
+//   ADMIN = 5,
+//   READ_ONLY,
+//   AUTHOR,
+// }
 
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["sports", "cooking"],
-  role: Role.ADMIN,
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: Role.ADMIN,
+// };
+
+// let favoriteActivities: string[];
+// favoriteActivities = ["sports"];
+
+// console.log(person.name);
+
+// person.hobbies.map((hob) => {
+//   console.log(hob.toUpperCase());
+// });
+
+// if (person.role === Role.ADMIN) {
+//   console.log("is author");
+// }
+
+type Combinable = number | string;
+type ConversionDescriptor = "as-numbers" | "as-text";
+
+const combine = (
+  input1: Combinable,
+  input2: Combinable,
+  resultConver: ConversionDescriptor
+) => {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  if (resultConver === "as-numbers") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 };
 
-let favoriteActivities: any[];
-favoriteActivities = ["sports"];
+const combinedAges = combine(30, 26, "as-numbers");
+console.log(combinedAges);
 
-console.log(person.name);
+const combinedStringAges = combine("30", "26", "as-numbers");
+console.log(combinedStringAges);
 
-person.hobbies.map((hob) => {
-  console.log(hob.toUpperCase());
-});
-
-if (person.role === Role.ADMIN) {
-  console.log("is author");
-}
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
